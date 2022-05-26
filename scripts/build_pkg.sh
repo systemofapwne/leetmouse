fe3a79d1e6820fc3ee1f8a5552f54e472734e05b
Klaus Zipfel <klaus@zipfel.family>
2022-05-26 21:08:16 +0200

    Integration of libfixmath into the build-process and some fixes

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -10,6 +10,9 @@ SRC_NAME=leetmouse-$VERSION
 # ########## Create output director
 mkdir -p "${ROOT}/pkg/build"
 
+# ########## Fetch external dependencies
+git submodule init && git submodule update
+
 # ########## Creates a compressed file for releases on github, mkpkg etc
 # Tar, copy, untar, tar.xz: I know, this is stupid, but atleast I can skip all git ignored files with that method and control the base-folder in the tar.xz file
 tar --exclude-vcs --exclude-vcs-ignores -cf $TEMP_DIR/tmp.tar -C "${ROOT}" .
@@ -20,7 +23,7 @@ mkdir -p $SRC_NAME
 tar -xf tmp.tar -C $SRC_NAME
 
 # HACK HACK HACK We purposedly ignored config.h earlier with the "--exclude-vcs --exclude-vcs-ignores" flags. Since config.h (if it exists) is in .gitignore, we need to add it manually.
-cp ${ROOT}/driver/config.h $SRC_NAME/driver/
+cp ${ROOT}/driver/config.h $SRC_NAME/driver/ > /dev/null 2>&1
 
 tar -cJf $SRC_NAME.tar.xz $SRC_NAME
 
