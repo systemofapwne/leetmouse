51cf4e2441a68ed90d1232288a34457ab0bd38cf
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-03-05 00:16:58 +0200

    Port acceleration code to fixed point math using fixedptc.h

--- driver/accel.c
+++ driver/accel.c
@@ -2,12 +2,13 @@
 
 #include "accel.h"
 #include "util.h"
-#include "float.h"
 #include "config.h"
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/time.h>
 #include <linux/string.h>   //strlen
+#include <linux/init.h>
+#include "fixedptc.h"
 
 //Needed for kernel_fpu_begin/end
 #include <linux/version.h>
@@ -21,14 +22,11 @@
 MODULE_AUTHOR("Christopher Williams <chilliams (at) gmail (dot) com>"); //Original idea of this module
 MODULE_AUTHOR("Klaus Zipfel <klaus (at) zipfel (dot) family>");         //Current maintainer
 
-//Converts a preprocessor define's value in "config.h" to a string - Suspect this to change in future version without a "config.h"
-#define _s(x) #x
-#define s(x) _s(x)
-
 //Convenient helper for float based parameters, which are passed via a string to this module (must be individually parsed via atof() - available in util.c)
-#define PARAM_F(param, default, desc)                           \
-    float g_##param = default;                                  \
-    static char* g_param_##param = s(default);                  \
+//TODO: Add parameter default value to string conversion
+#define PARAM_F(param, default, desc)              \
+    fixedpt g_##param = default;                                \
+    static char* g_param_##param = "";                 \
     module_param_named(param, g_param_##param, charp, 0644);    \
     MODULE_PARM_DESC(param, desc);
 
@@ -55,19 +53,23 @@ PARAM_F(SensitivityCap, SENS_CAP,       "Cap maximum sensitivity.");
 PARAM_F(Offset,         OFFSET,         "Mouse base sensitivity.");
 //PARAM_F(Power,          XXX,            "");           //Not yet implemented
 PARAM_F(PostScaleX,     POST_SCALE_X,   "Postscale X-Axis after applying acceleration.");
-PARAM_F(PostScaleY,     POST_SCALE_Y,   "Postscale >-Axis after applying acceleration.");
-//PARAM_F(AngleAdjustment,XXX,            "");           //Not yet implemented. Douptful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
-//PARAM_F(AngleSnapping,  XXX,            "");           //Not yet implemented. Douptful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
-PARAM_F(ScrollsPerTick, SCROLLS_PER_TICK,"Amount of lines to scroll per scroll-wheel tick.");
+PARAM_F(PostScaleY,     POST_SCALE_Y,   "Postscale Y-Axis after applying acceleration.");
+//PARAM_F(AngleAdjustment,XXX,            "");           //Not yet implemented. Doubtful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
+//PARAM_F(AngleSnapping,  XXX,            "");           //Not yet implemented. Doubtful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
+PARAM_F(ScrollsPerTick, SCROLLS_PER_TICK, "Amount of lines to scroll per scroll-wheel tick.");
 
+void update_param(const char *str, fixedpt *result) {
+    //TODO: Add parameter updating here
+    *result = *result;
+}
 
 // Updates the acceleration parameters. This is purposely done with a delay!
 // First, to not hammer too much the logic in "accelerate()", which is called VERY OFTEN!
 // Second, to fight possible cheating. However, this can be OFC changed, since we are OSS...
-#define PARAM_UPDATE(param) atof(g_param_##param, strlen(g_param_##param) , &g_##param);
+#define PARAM_UPDATE(param) update_param(g_param_##param, &g_##param);
 
 static ktime_t g_next_update = 0;
-INLINE void updata_params(ktime_t now)
+INLINE void update_params(ktime_t now)
 {
     if(!g_update) return;
     if(now < g_next_update) return;
@@ -91,146 +93,83 @@ INLINE void updata_params(ktime_t now)
 // Acceleration happens here
 int accelerate(int *x, int *y, int *wheel)
 {
-	float delta_x, delta_y, delta_whl, ms, rate, accel_sens;
-    static long buffer_x = 0;
-    static long buffer_y = 0;
-    static long buffer_whl = 0;
-    //Static float assignment should happen at compile-time and thus should be safe here. However, avoid non-static assignment of floats outside kernel_fpu_begin()/kernel_fpu_end()
-	static float carry_x = 0.0f;
-    static float carry_y = 0.0f;
-    static float carry_whl = 0.0f;
-	static float last_ms = 1.0f;
-	static ktime_t last;
-	ktime_t now;
+    fixedpt delta_x, delta_y, delta_whl, ms, rate, accel_sens;
+    static fixedpt carry_x = fixedpt_rconst(0.0);
+    static fixedpt carry_y = fixedpt_rconst(0.0);
+    static fixedpt carry_whl = fixedpt_rconst(0.0);
+    static fixedpt last_ms = fixedpt_rconst(1.0);
+    static ktime_t last;
+    ktime_t now;
     int status = 0;
 
-    // We can only safely use the FPU in an IRQ event when this returns 1.
-    // Not taking care for this interfered with BTRFS on my machine (which also uses kernel_fpu_begin/kernel_fpu_end) and lead to data corruption. And I guess, the same would be true for raid6 (both use kernel_fpu_begin/kernel_fpu_end).
-    if(!irq_fpu_usable()){
-        // Buffer mouse deltas for next (valid) IRQ
-        buffer_x += *x;
-        buffer_y += *y;
-        buffer_whl += *wheel;
-        return -EBUSY;
-    }
-
-//We are going to use the FPU within the kernel. So we need to safely switch context during all FPU processing in order to not corrupt the userspace FPU state
-//Note: Avoid any function calls (https://yarchive.net/comp/linux/kernel_fp.html - Torvalds: "It all has to be stuff that gcc can do in-line,without any function calls.")
-//This is why we use the "INLINE" pre-processor directive (defined in util.h), which expands to "__attribute__((always_inline)) inline" in order to force gcc to inline the functions defined in float.h
-//Not doing this caused the FPU state to get randomly screwed up (https://github.com/systemofapwne/leetmouse/issues/4), making the cursor to get stuck on the left screen. Especially when playing certain videos in the browser.
-kernel_fpu_begin();
     accel_sens = g_Sensitivity;
 
-    delta_x = (float) (*x);
-    delta_y = (float) (*y);
-    delta_whl = (float) (*wheel);
-
-    // When compiled with mhard-float, I noticed that casting to float sometimes returns invalid values, especially when playing this video in brave/chrome/chromium
-    // https://sps-tutorial.com/was-ist-eine-sps/ or https://www.youtube.com/watch?v=tjT9gt0dArQ or https://www.ginx.tv/en/cs-go/cs-go-trusted-mode-how-to-enable-third-party-software
-    // Here we check, if casting did work out.
-    if(!((int) delta_x == *x && (int) delta_y == *y && (int) delta_whl == *wheel)){
-        // Buffer mouse deltas for next (valid) IRQ
-        buffer_x += *x;
-        buffer_y += *y;
-        buffer_whl += *wheel;
-        // Jump out of kernel_fpu_begin
-        status = -EFAULT;
-        printk("LEETMOUSE: First float-trap triggered. Should very very rarely happen, if at all");
-        goto exit;
-    }
-
-    //Add buffer values, if present, and reset buffer
-    delta_x += (float) buffer_x; buffer_x = 0;
-    delta_y += (float) buffer_y; buffer_y = 0;
-    delta_whl += (float) buffer_whl; buffer_whl = 0;
+    delta_x = fixedpt_fromint(*x);
+    delta_y = fixedpt_fromint(*y);
+    delta_whl = fixedpt_fromint(*wheel);
 
     //Calculate frametime
     now = ktime_get();
-    ms = (now - last)/(1000*1000);
+    ms = fixedpt_div(fixedpt_fromint(now - last), fixedpt_fromint(1000*1000));
     last = now;
-    if(ms < 1) ms = last_ms;    //Sometimes, urbs appear bunched -> Beyond µs resolution so the timing reading is plain wrong. Fallback to last known valid frametime
-    if(ms > 100) ms = 100;      //Original InterAccel has 200 here. RawAccel rounds to 100. So do we.
+    if(ms < fixedpt_rconst(1.0)) ms = last_ms;        //Sometimes, urbs appear bunched -> Beyond µs resolution so the timing reading is plain wrong. Fallback to last known valid frametime
+    if(ms > fixedpt_rconst(100.0)) ms = fixedpt_rconst(100.0);    //Original InterAccel has 200 here. RawAccel rounds to 100. So do we.
     last_ms = ms;
 
     //Update acceleration parameters periodically
-    updata_params(now);
+    update_params(now);
 
     //Prescale
-    delta_x *= g_PreScaleX;
-    delta_y *= g_PreScaleY;
+    delta_x = fixedpt_mul(delta_x, g_PreScaleX);
+    delta_y = fixedpt_mul(delta_y, g_PreScaleY);
 
     //Calculate velocity (one step before rate, which divides rate by the last frametime)
-    rate = delta_x * delta_x + delta_y * delta_y;
-    B_sqrt(&rate);
+    rate = fixedpt_add(fixedpt_mul(delta_x, delta_x), fixedpt_mul(delta_y, delta_y));
+    rate = fixedpt_sqrt(rate);
 
     //Apply speedcap
-    if(g_SpeedCap != 0){
+    if(g_SpeedCap != fixedpt_rconst(0.0)) {
         if (rate >= g_SpeedCap) {
-            delta_x *= g_SpeedCap / rate;
-            delta_y *= g_SpeedCap / rate;
+            delta_x = fixedpt_mul(delta_x, fixedpt_div(g_SpeedCap, rate));
+            delta_y = fixedpt_mul(delta_y, fixedpt_div(g_SpeedCap, rate));
             rate = g_SpeedCap;
         }
     }
 
     //Calculate rate from travelled overall distance and add possible rate offsets
-    rate /= ms;
-    rate -= g_Offset;
+    rate = fixedpt_div(rate, ms);
+    rate = fixedpt_sub(rate, g_Offset);
 
     //TODO: Add different acceleration styles
     //Apply linear acceleration on the sensitivity if applicable and limit maximum value
-    if(rate > 0){
-        rate *= g_Acceleration;
-        accel_sens += rate;
+    if(rate > fixedpt_rconst(0.0)){
+        rate = fixedpt_mul(rate, g_Acceleration);
+        accel_sens = fixedpt_add(accel_sens, rate);
     }
-    if(g_SensitivityCap > 0 && accel_sens >= g_SensitivityCap){
+    if(g_SensitivityCap > fixedpt_rconst(0.0) && accel_sens >= g_SensitivityCap){
         accel_sens = g_SensitivityCap;
     }
 
     //Actually apply accelerated sensitivity, allow post-scaling and apply carry from previous round
-    accel_sens /= g_Sensitivity;
-    delta_x *= accel_sens;
-    delta_y *= accel_sens;
-    delta_x *= g_PostScaleX;
-    delta_y *= g_PostScaleY;
-    delta_whl *= g_ScrollsPerTick/3.0f;
-    delta_x += carry_x;
-    delta_y += carry_y;
+    accel_sens = fixedpt_div(accel_sens, g_Sensitivity);
+    delta_x = fixedpt_mul(delta_x, accel_sens);
+    delta_y = fixedpt_mul(delta_y, accel_sens);
+    delta_x = fixedpt_mul(delta_x, g_PostScaleX);
+    delta_y = fixedpt_mul(delta_y, g_PostScaleY);
+    delta_whl = fixedpt_mul(delta_whl, fixedpt_div(g_ScrollsPerTick, fixedpt_rconst(3.0)));
+    delta_x = fixedpt_add(delta_x, carry_x); 
+    delta_y = fixedpt_add(delta_y, carry_y); 
     if((delta_whl < 0 && carry_whl < 0) || (delta_whl > 0 && carry_whl > 0)) //Only apply carry to the wheel, if it shares the same sign
         delta_whl += carry_whl;
 
-    //Last check for validity
-    if(!(isfinite(&delta_x) && isfinite(&delta_y) && isfinite(&delta_whl))){
-        // Buffer mouse deltas for next (valid) IRQ
-        buffer_x += *x;
-        buffer_y += *y;
-        buffer_whl += *wheel;
-        // Jump out of kernel_fpu_begin
-        printk("LEETMOUSE: Acceleration of NaN value");
-        status = -EFAULT;
-        goto exit;
-    }
-
-    //Cast back to int
-    *x = Leet_round(&delta_x);
-    *y = Leet_round(&delta_y);
-    *wheel = Leet_round(&delta_whl);
-
-    // Very last trap. This should NEVER get triggered. Buf if the FPU state gets screwed up "somehow", it seems like the floats get casted to MIN_INT (-2147483648). So we trap this edge case
-    if(*x == -2147483648 || *y == -2147483648 || *wheel == -2147483648){
-        // Jump out of kernel_fpu_begin
-        printk("LEETMOUSE: Final float-trap triggered. This should NEVER happen!");
-        status = -EFAULT;
-        goto exit;
-    }
+    *x = fixedpt_toint(delta_x);
+    *y = fixedpt_toint(delta_y);
+    *wheel = fixedpt_toint(delta_whl);
 
     //Save carry for next round
-    carry_x = delta_x - *x;
-    carry_y = delta_y - *y;
-    carry_whl = delta_whl - *wheel;
-    
-exit:
-//We stopped using the FPU: Switch back context again
-kernel_fpu_end();
+    carry_x = fixedpt_sub(delta_x, fixedpt_fromint(*x));
+    carry_y = fixedpt_sub(delta_y, fixedpt_fromint(*y));
+    carry_whl = fixedpt_sub(delta_whl, fixedpt_fromint(*wheel));
 
     return status;
 }
