e185bcc7ffc233a374fa4c614013bfcdfb27f3be
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-03-12 18:15:17 +0200

    Convert to libfixmath

--- driver/accel.c
+++ driver/accel.c
@@ -8,7 +8,7 @@
 #include <linux/time.h>
 #include <linux/string.h>   //strlen
 #include <linux/init.h>
-#include "fixedptc.h"
+#include "libfixmath/libfixmath/fixmath.h"
 
 //Needed for kernel_fpu_begin/end
 #include <linux/version.h>
@@ -22,11 +22,15 @@
 MODULE_AUTHOR("Christopher Williams <chilliams (at) gmail (dot) com>"); //Original idea of this module
 MODULE_AUTHOR("Klaus Zipfel <klaus (at) zipfel (dot) family>");         //Current maintainer
 
-//Convenient helper for float based parameters, which are passed via a string to this module (must be individually parsed via atof() - available in util.c)
-//TODO: Add parameter default value to string conversion
+//Converts a preprocessor define's value in "config.h" to a string - Suspect this to change in future version without a "config.h"
+#define _s(x) #x
+#define s(x) _s(x)
+
+//Convenient helper for fixed point parameters, which are passed via a string module param and parsed using libfixmath's fix16_from_str
+//TODO: Add proper fixed point to string conversion of defaults. Validity checking in update_param can be removed once this is done.
 #define PARAM_F(param, default, desc)              \
-    fixedpt g_##param = default;                                \
-    static char* g_param_##param = "";                 \
+    fix16_t g_##param = default;                                \
+    static char* g_param_##param = s(default);                 \
     module_param_named(param, g_param_##param, charp, 0644);    \
     MODULE_PARM_DESC(param, desc);
 
@@ -54,13 +58,14 @@ PARAM_F(Offset,         OFFSET,         "Mouse base sensitivity.");
 //PARAM_F(Power,          XXX,            "");           //Not yet implemented
 PARAM_F(PostScaleX,     POST_SCALE_X,   "Postscale X-Axis after applying acceleration.");
 PARAM_F(PostScaleY,     POST_SCALE_Y,   "Postscale Y-Axis after applying acceleration.");
-//PARAM_F(AngleAdjustment,XXX,            "");           //Not yet implemented. Doubtful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
-//PARAM_F(AngleSnapping,  XXX,            "");           //Not yet implemented. Doubtful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
+//PARAM_F(AngleAdjustment,XXX,            "");           //Not yet implemented. Douptful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
+//PARAM_F(AngleSnapping,  XXX,            "");           //Not yet implemented. Douptful, if I will ever add it - Not very useful and needs me to implement trigonometric functions from scratch in C.
 PARAM_F(ScrollsPerTick, SCROLLS_PER_TICK, "Amount of lines to scroll per scroll-wheel tick.");
 
-void update_param(const char *str, fixedpt *result) {
-    //TODO: Add parameter updating here
-    *result = *result;
+void update_param(const char *str, fix16_t *result) {
+    fix16_t new_value = fix16_from_str(str);
+    if (new_value != fix16_overflow)
+        *result = new_value;
 }
 
 // Updates the acceleration parameters. This is purposely done with a delay!
@@ -91,85 +96,92 @@ INLINE void update_params(ktime_t now)
 // ########## Acceleration code
 
 // Acceleration happens here
-int accelerate(int *x, int *y, int *wheel)
+void accelerate(int *x, int *y, int *wheel)
 {
-    fixedpt delta_x, delta_y, delta_whl, ms, rate, accel_sens;
-    static fixedpt carry_x = fixedpt_rconst(0.0);
-    static fixedpt carry_y = fixedpt_rconst(0.0);
-    static fixedpt carry_whl = fixedpt_rconst(0.0);
-    static fixedpt last_ms = fixedpt_rconst(1.0);
+    fix16_t delta_x, delta_y, delta_whl, ms, rate, accel_sens;
+    static fix16_t carry_x = F16(0.0);
+    static fix16_t carry_y = F16(0.0);
+    static fix16_t carry_whl = F16(0.0);
+    static fix16_t last_ms = F16(1.0);
     static ktime_t last;
     ktime_t now;
-    int status = 0;
 
     accel_sens = g_Sensitivity;
 
-    delta_x = fixedpt_fromint(*x);
-    delta_y = fixedpt_fromint(*y);
-    delta_whl = fixedpt_fromint(*wheel);
+    delta_x = fix16_from_int(*x);
+    delta_y = fix16_from_int(*y);
+    delta_whl = fix16_from_int(*wheel);
 
     //Calculate frametime
     now = ktime_get();
-    ms = fixedpt_div(fixedpt_fromint(now - last), fixedpt_fromint(1000*1000));
+    ms = fix16_div(fix16_from_int(now - last), fix16_from_int(1000*1000));
     last = now;
-    if(ms < fixedpt_rconst(1.0)) ms = last_ms;        //Sometimes, urbs appear bunched -> Beyond µs resolution so the timing reading is plain wrong. Fallback to last known valid frametime
-    if(ms > fixedpt_rconst(100.0)) ms = fixedpt_rconst(100.0);    //Original InterAccel has 200 here. RawAccel rounds to 100. So do we.
+    if(ms < fix16_one) ms = last_ms;        //Sometimes, urbs appear bunched -> Beyond µs resolution so the timing reading is plain wrong. Fallback to last known valid frametime
+    if(ms > F16(100.0)) ms = F16(100.0);    //Original InterAccel has 200 here. RawAccel rounds to 100. So do we.
     last_ms = ms;
 
     //Update acceleration parameters periodically
     update_params(now);
 
     //Prescale
-    delta_x = fixedpt_mul(delta_x, g_PreScaleX);
-    delta_y = fixedpt_mul(delta_y, g_PreScaleY);
+    delta_x = fix16_mul(delta_x, g_PreScaleX);
+    delta_y = fix16_mul(delta_y, g_PreScaleY);
 
     //Calculate velocity (one step before rate, which divides rate by the last frametime)
-    rate = fixedpt_add(fixedpt_mul(delta_x, delta_x), fixedpt_mul(delta_y, delta_y));
-    rate = fixedpt_sqrt(rate);
+    rate = fix16_add(fix16_mul(delta_x, delta_x), fix16_mul(delta_y, delta_y));
+    rate = fix16_sqrt(rate);
 
     //Apply speedcap
-    if(g_SpeedCap != fixedpt_rconst(0.0)) {
+    if(g_SpeedCap != F16(0.0)) {
         if (rate >= g_SpeedCap) {
-            delta_x = fixedpt_mul(delta_x, fixedpt_div(g_SpeedCap, rate));
-            delta_y = fixedpt_mul(delta_y, fixedpt_div(g_SpeedCap, rate));
+            delta_x = fix16_mul(delta_x, fix16_div(g_SpeedCap, rate));
+            delta_y = fix16_mul(delta_y, fix16_div(g_SpeedCap, rate));
             rate = g_SpeedCap;
         }
     }
 
     //Calculate rate from travelled overall distance and add possible rate offsets
-    rate = fixedpt_div(rate, ms);
-    rate = fixedpt_sub(rate, g_Offset);
+    rate = fix16_div(rate, ms);
+    rate = fix16_sub(rate, g_Offset);
 
     //TODO: Add different acceleration styles
     //Apply linear acceleration on the sensitivity if applicable and limit maximum value
-    if(rate > fixedpt_rconst(0.0)){
-        rate = fixedpt_mul(rate, g_Acceleration);
-        accel_sens = fixedpt_add(accel_sens, rate);
+    if(rate > F16(0.0)){
+        rate = fix16_mul(rate, g_Acceleration);
+        accel_sens = fix16_add(accel_sens, rate);
     }
-    if(g_SensitivityCap > fixedpt_rconst(0.0) && accel_sens >= g_SensitivityCap){
+    if(g_SensitivityCap > F16(0.0) && accel_sens >= g_SensitivityCap){
         accel_sens = g_SensitivityCap;
     }
 
     //Actually apply accelerated sensitivity, allow post-scaling and apply carry from previous round
-    accel_sens = fixedpt_div(accel_sens, g_Sensitivity);
-    delta_x = fixedpt_mul(delta_x, accel_sens);
-    delta_y = fixedpt_mul(delta_y, accel_sens);
-    delta_x = fixedpt_mul(delta_x, g_PostScaleX);
-    delta_y = fixedpt_mul(delta_y, g_PostScaleY);
-    delta_whl = fixedpt_mul(delta_whl, fixedpt_div(g_ScrollsPerTick, fixedpt_rconst(3.0)));
-    delta_x = fixedpt_add(delta_x, carry_x); 
-    delta_y = fixedpt_add(delta_y, carry_y); 
-    if((delta_whl < 0 && carry_whl < 0) || (delta_whl > 0 && carry_whl > 0)) //Only apply carry to the wheel, if it shares the same sign
-        delta_whl += carry_whl;
+    accel_sens = fix16_div(accel_sens, g_Sensitivity);
+    //Comments below are (dumb) examples of how to add debug code.
+    //char delta_x_str[13];
+    //char delta_x_result_str[13];
+    //fix16_to_str(delta_x, delta_x_str, 5);
+    delta_x = fix16_mul(delta_x, accel_sens);
+    //fix16_to_str(delta_x, delta_x_result_str, 5);
+    //printk("Before: %s, After: %s", delta_x_str, delta_x_result_str);
+    delta_y = fix16_mul(delta_y, accel_sens);
+    delta_x = fix16_mul(delta_x, g_PostScaleX);
+    delta_y = fix16_mul(delta_y, g_PostScaleY);
 
-    *x = fixedpt_toint(delta_x);
-    *y = fixedpt_toint(delta_y);
-    *wheel = fixedpt_toint(delta_whl);
+    delta_whl = fix16_mul(delta_whl, fix16_div(g_ScrollsPerTick, F16(3.0)));
+    delta_x = fix16_add(delta_x, carry_x);
+    delta_y = fix16_add(delta_y, carry_y);
+    if((delta_whl < 0 && carry_whl < 0) || (delta_whl > 0 && carry_whl > 0)) //Only apply carry to the wheel, if it shares the same sign
+        delta_whl = fix16_add(delta_whl, carry_whl);
+
+    if (delta_x == fix16_overflow || delta_y == fix16_overflow || delta_whl == fix16_overflow)
+        printk("LEETMOUSE: Arithmetic overflow in acceleration math.");
+
+    *x = fix16_to_int(delta_x);
+    *y = fix16_to_int(delta_y);
+    *wheel = fix16_to_int(delta_whl);
 
     //Save carry for next round
-    carry_x = fixedpt_sub(delta_x, fixedpt_fromint(*x));
-    carry_y = fixedpt_sub(delta_y, fixedpt_fromint(*y));
-    carry_whl = fixedpt_sub(delta_whl, fixedpt_fromint(*wheel));
-
-    return status;
+    carry_x = fix16_sub(delta_x, fix16_from_int(*x));
+    carry_y = fix16_sub(delta_y, fix16_from_int(*y));
+    carry_whl = fix16_sub(delta_whl, fix16_from_int(*wheel));
 }
--- driver/usbmouse.c
+++ driver/usbmouse.c
@@ -89,11 +89,10 @@ static void usb_mouse_irq(struct urb *urb)
         input_report_key(dev, BTN_MIDDLE, btn & 0x04);
         input_report_key(dev, BTN_SIDE,   btn & 0x08);
         input_report_key(dev, BTN_EXTRA,  btn & 0x10);
-        if(!accelerate(&x,&y,&wheel)){
-            input_report_rel(dev, REL_X,     x);
-            input_report_rel(dev, REL_Y,     y);
-            input_report_rel(dev, REL_WHEEL, wheel);
-        }
+        accelerate(&x,&y,&wheel);
+        input_report_rel(dev, REL_X,     x);
+        input_report_rel(dev, REL_Y,     y);
+        input_report_rel(dev, REL_WHEEL, wheel);
     }
                                                                 //Leetmouse Mod END
 
