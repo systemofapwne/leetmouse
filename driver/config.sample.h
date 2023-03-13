f79954e1e06eb05183768592ed2a28af883845ca
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-03-13 02:44:15 +0200

    Convert sample config to libfixmath

--- driver/config.sample.h
+++ driver/config.sample.h
@@ -1,4 +1,4 @@
-#include "fixedptc.h"
+#include "libfixmath/libfixmath/fix16.h"
 // Maximum number of packets allowed to be sent from the mouse at once. Linux's default value is 8, which at
 // least causes EOVERFLOW for my mouse (SteelSeries Rival 600). Increase this, if 'dmesg -w' tells you to!
 #define BUFFER_SIZE 8
@@ -10,22 +10,22 @@
  */
 
 // Changes behaviour of the scroll-wheel. Default is 3.0f
-#define SCROLLS_PER_TICK fixedpt_rconst(3.0)
+#define SCROLLS_PER_TICK F16(3.0)
 
 // Emulate Windows' "Enhanced Pointer Precision" for my mouse (1000 Hz) by approximating it with a linear accel
-#define SENSITIVITY fixedpt_rconst(1.0)
-#define ACCELERATION fixedpt_rconst(0.05)
-#define SENS_CAP fixedpt_rconst(3.0)
-#define OFFSET fixedpt_rconst(0.0)
-#define POST_SCALE_X fixedpt_rconst(1.0)
-#define POST_SCALE_Y fixedpt_rconst(1.0)
-#define SPEED_CAP fixedpt_rconst(0.0)
+#define SENSITIVITY F16(1.0)
+#define ACCELERATION F16(0.05)
+#define SENS_CAP F16(3.0)
+#define OFFSET F16(0.0)
+#define POST_SCALE_X F16(1.0)
+#define POST_SCALE_Y F16(1.0)
+#define SPEED_CAP F16(0.0)
 
 // Prescaler for different DPI values. 1.0f at 400 DPI. To adjust it for <your_DPI>, calculate 400/your_DPI
 
 // Generic @ 400 DPI
-#define PRE_SCALE_X fixedpt_rconst(1.0)
-#define PRE_SCALE_Y fixedpt_rconst(1.0)
+#define PRE_SCALE_X F16(1.0)
+#define PRE_SCALE_Y F16(1.0)
 
 // Steelseries Rival 110 @ 7200 DPI
 //#define PRE_SCALE_X 0.0555555f
