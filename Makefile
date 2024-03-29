f80996f62b3eb7ab3e9c296393ff38904b0f69c1
Samuel Sampaio <samukasampaio@hotmail.com>
2024-03-29 17:10:51 -0300

    PR #28 Funfando com letmouse-gui

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ DRIVERDIR?=$(shell pwd)/driver
 MODULEDIR?=/lib/modules/$(shell uname -r)/kernel/drivers/usb
 
 DKMS_NAME?=leetmouse-driver
-DKMS_VER?=0.9.0
+DKMS_VER?=0.9.5
 
 
 .PHONY: driver
---
+++ rm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DKMS_VER=$(cat "./Makefile" | grep -oP "(?<=DKMS_VER\?=)[0-9\.]+")
+
+
+# Desvincular todos os mouses do driver
+sudo /usr/lib/udev/leetmouse_manage unbind_all
+
+# Desinstalar o driver
+sudo dkms remove -m leetmouse-driver -v $DKMS_VER
+sudo make remove_dkms && sudo make udev_uninstall
